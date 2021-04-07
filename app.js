759aba84b9417fc520ad14cd9b4f68de2119bdea
des-sheehan <77647491+des-sheehan@users.noreply.github.com>
2021-04-07 11:28:59 +0100

    Version 3
    Correct Commit.
    
    Transition effects added.
    
    Still need to find why white space between letters causes missed value to increase.

--- app.js
+++ app.js
@@ -3,10 +3,9 @@
 
 const overlay = document.getElementById('overlay');
 const qwerty = document.getElementById('qwerty');
-const phrase = document.getElementById('phrase');
+const phrase = document.querySelector('#phrase ul');
 
-const startButton = document.getElementsByClassName('btn__reset')[0];
-const startOverlay = document.getElementsByClassName('start');
+const startButton = document.querySelector('#overlay a');
 const scoreboard = document.querySelector('#scoreboard ol')
 const title = overlay.querySelector('.title');
 
@@ -36,6 +35,9 @@ const getRandomInt = (min, max) => {
 //select a random array number
 const randomArrayNumber = getRandomInt(0, phrases.length);
 
+// set overkay to start
+  
+
 //Start Button Event Listener to hide Overlay
     startButton.addEventListener('click', () => {
       overlay.style.display = 'none';
@@ -54,9 +56,8 @@ const phraseArray = getRandomPhraseAsArray(phrases);
 // // adds the letters of a string to the display
 const addPhraseToDisplay = arr => {
   for (i = 0; i < phraseArray.length; i++) {
-    var ul = document.getElementById('phrase');
     var li = document.createElement('LI');
-    ul.appendChild(li);
+    phrase.appendChild(li);
     li.append(phraseArray[i]) 
     //adds non-empty strings to the letter class, else to the space class
     if ( phraseArray[i] !== ' ') {
@@ -79,8 +80,12 @@ const checkLetter = (qwertyButton) => {
       letters[i].classList.add('show')
       match = letters[i].textContent;
     } 
+    checkWin();
   }
   // This fires an error even wen scoreboard has no Child Nodes
+
+// This needs to be refined, a value for null is achieved by not even clicking a letter.
+// clicking anywhere give no match and = null = missed++
 
   // Increase Missed Value and remove Heart
   if ( match === null ) {
@@ -90,7 +95,7 @@ const checkLetter = (qwertyButton) => {
   }
 }   
 
- //list for the onscreen keyboard to be clicked
+ //listen for the onscreen keyboard to be clicked
  qwerty.addEventListener('click', e => {
   var button = e.target;
   if (button.tagName === 'BUTTON') {
@@ -103,29 +108,37 @@ const checkLetter = (qwertyButton) => {
 });
 
 // want to stop buttons being pressed when game is over (if overlay doesn't hide them)
-
-// //need to actually run the function!!! 
-// const win = ( letters.length === correct.length );
-// const lose = ( missed > 4 )
+// i could maybe let the condition for winning be a variable 'win'
+// then when adding the classname, use the variable.textContent.
+// this way I could shorten all this repeating code.
 
 //check if the game has been won or lost
-const checkWin = () => {
-    if ( letters.length === correct.length ) {
+const checkWin = () => {  
+  var win = correct.length === letters.length;
+  var lose = missed >= 4;
+
+  if ( win ) {
       overlay.style.display = 'flex';
       overlay.classList.remove('start');
       overlay.classList.add('win');
       title.textContent = 'Congratulations!'
-    } else if ( missed > 4 ) {
-        overlay.style.display = 'flex'
-        overlay.classList.remove('start');
-        overlay.classList.add('lose');
-        title.textContent = 'Better Luck Next Time!'
-    }
+      playAgain();
+  } else if ( lose ) {
+    overlay.style.display = 'flex'
+    overlay.classList.remove('start');
+    overlay.classList.add('lose');
+    title.textContent = 'Better Luck Next Time!'
+    playAgain();
+      }
   }
 
-checkWin();
-
-//listen for the start game button to be pressed
-// resetButton.addEventListener('click', e => {
-
-// });
+const playAgain = () => {
+  overlay.removeChild(startButton);
+  let resetButton = document.createElement('BUTTON');
+  overlay.appendChild(resetButton);
+  resetButton.textContent = 'Play Again';
+  resetButton.classList.add('btn__reset');
+  resetButton.addEventListener('click', e => {
+    window.location.reload();
+  }
+)};
